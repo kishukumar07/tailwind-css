8af3b7b58fb47b01eae34f67bd87889e32d0699a
kishukumar07 <kishukumars082@gmail.com>
2025-07-11 19:36:12 +0530

    @apply directive in tailwind

---
+++ app/03directives/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <button className="upload-btn  text-cyan-900">Button</button>
+      <h1 className="text-amber-300">hello</h1>
+    </div>
+  );
+}
+
+export default page;
